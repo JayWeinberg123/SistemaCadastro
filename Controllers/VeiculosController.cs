46836b895a67229b3b0d461e32780298818a1915
JayWeinberg123 <lucas_souzasjc@hotmail.com>
2024-04-19 14:01:48 -0300

    feat: Sistema de Cadastro V2

---
+++ Controllers/VeiculosController.cs
@@ -0,0 +1,113 @@
+﻿using SistemaCadastro.Models;
+using SistemaCadastro.Repo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+
+namespace SistemaCadastro.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private VeiculosRepo _repositorio;
+
+
+
+        public ActionResult ObterVeiculos()
+        {
+            _repositorio = new VeiculosRepo();
+
+            ModelState.Clear();
+            return View(_repositorio.ObterVeiculos());
+        }
+
+        [HttpGet]
+        public ActionResult IncluirVeiculo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IncluirVeiculo(Veiculos veiculoObj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repositorio = new VeiculosRepo();
+
+                    if (_repositorio.IncluirVeiculo(veiculoObj))
+                    {
+                        ViewBag.Mensagem = "Veículo Cadastrado com Sucesso!";
+                    }
+                }
+
+                return RedirectToAction("ObterVeiculos");
+            }
+            catch (Exception)
+            {
+
+                return View("ObterVeiculos");
+            }
+
+        }
+
+
+
+        [HttpGet]
+        public ActionResult AtualizarVeiculo(int id)
+        {
+            _repositorio = new VeiculosRepo();
+            return View(_repositorio.ObterVeiculos().Find(t => t.VeiculosId == id));
+        }
+
+        [HttpPost]
+        public ActionResult AtualizarVeiculo(int id, Veiculos veiculoObj)
+        {
+            try
+            {
+                _repositorio = new VeiculosRepo();
+                _repositorio.AtualizarVeiculo(veiculoObj);
+                if (_repositorio.AtualizarVeiculo(veiculoObj))
+                {
+                    ViewBag.Mensagem = "Veículo atualizado com sucesso!!";
+                }
+                return RedirectToAction("ObterVeiculos");
+            }
+            catch (Exception)
+            {
+                return View("ObterVeiculos");
+            }
+        }
+
+        public ActionResult ExcluirVeiculo(int id)
+        {
+            try
+            {
+                _repositorio = new VeiculosRepo();
+                if (_repositorio.ExcluirVeiculo(id))
+                {
+                    ViewBag.Mensagem = "Time excluído com sucesso";
+                }
+
+                return RedirectToAction("ObterVeiculos");
+            }
+            catch (Exception)
+            {
+                return View("ObterVeiculos");
+            }
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ Models/Veiculos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SistemaCadastro.Models
+
+{
+
+    public class Veiculos
+    {
+
+        [Display(Name = "Id")]
+        public int VeiculosId { get; set; }
+
+        [Required(ErrorMessage = "Informe a PLACA")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Informe a RENAVAM")]
+        public int Renavam { get; set; }
+
+        [Required(ErrorMessage = "Informe o CHASSI")]
+        public string Chassi { get; set; }
+
+        [Required(ErrorMessage = "Informe o MOTOR")]
+        public string Motor { get; set; }
+
+        [Required(ErrorMessage = "Informe o MARCA")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "Informe a MODELO")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Informe o tipo de COMBUSTÍVEL")]
+        public string Combustivel { get; set; }
+
+        [Required(ErrorMessage = "Informe o tipo de COR")]
+        public string Cor { get; set; }
+
+        [Required(ErrorMessage = "Informe a ANO")]
+        public int Ano { get; set; }
+
+        [Required(ErrorMessage = "Informe o STATUS")]
+        public string Status { get; set; }
+
+        
+    }
+
+}
